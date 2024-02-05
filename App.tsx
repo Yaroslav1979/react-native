ede58c4b3e1b075ebb93d2fd3ba9f5009ec7b7d8
arturzinin <a.zinin@goit.ua>
2024-02-05 14:47:20 +0200

    block 3

--- App.tsx
+++ App.tsx
@@ -1,41 +1,11 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import LoginPage from './src/screen/Auth/Login/index';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Раді тебе вітати!</Text>
-        <Text>
-          Кожен пухнастик заслуговує на дбайливих господарів.Ми допоможемо тобі
-          знайти друга.
-        </Text>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>Вхід</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Реєстрація</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <View>
-          <TextInput />
-        </View>
-        <View>
-          <TextInput />
-        </View>
-      </View>
-      <TouchableOpacity>
-        <Text>Увійти</Text>
-      </TouchableOpacity>
+      <LoginPage />
     </SafeAreaView>
   );
 }
