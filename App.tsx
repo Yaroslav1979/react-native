f9938a05f9e847abef92526b4f62c9ee6b3e2386
arturzinin <a.zinin@goit.ua>
2024-02-18 18:39:52 +0200

    add pets list

--- App.tsx
+++ App.tsx
@@ -2,8 +2,26 @@ import React, {useEffect} from 'react';
 import {DevSettings, NativeModules, SafeAreaView} from 'react-native';
 import RootNavigation from './src/navigation';
 import 'react-native-gesture-handler';
+// import {firebase} from '@react-native-firebase/firestore';
+// import {animals} from './animals_list.js';
 
 function App(): React.JSX.Element {
+  // const db = firebase.firestore();
+  //
+  // const uploadAnimalsToFirestore = async animals => {
+  //   const collectionRef = db.collection('animals');
+  //
+  //   for (const animal of animals) {
+  //     await collectionRef
+  //       .add(animal)
+  //       .then(docRef => {
+  //         console.log(`Document written with ID: ${docRef.id}`);
+  //       })
+  //       .catch(error => {
+  //         console.error('Error adding document: ', error);
+  //       });
+  //   }
+  // };
   useEffect(() => {
     if (__DEV__) {
       DevSettings.addMenuItem('Debugging With debugger', () => {
@@ -13,6 +31,8 @@ function App(): React.JSX.Element {
         NativeModules.DevSettings.setIsDebuggingRemotely(false);
       });
     }
+
+    // uploadAnimalsToFirestore(animals);
   }, []);
   return (
     <SafeAreaView style={{flex: 1}}>
